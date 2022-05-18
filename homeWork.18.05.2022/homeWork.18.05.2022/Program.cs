090103dabcab9c7cc064bcaeaa9fb2b9c647bc61
khlzemil <emilmg@code.edu.az>
2022-05-18 11:49:37 +0400

    Added HomeTask

---
+++ homeWork.18.05.2022/homeWork.18.05.2022/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Program
+{
+    public static void Main()
+    {
+        int[] firstArr = {2,3,4,7};
+        int[] secondArr = {2,4};
+        Array.Sort(secondArr);
+        Array.Sort(firstArr);
+
+
+        Console.WriteLine(IsArrayContains(firstArr, secondArr));
+
+    }
+
+
+    static bool IsArrayContains(int[] firstArr, int[] secArr)
+    {
+        
+
+        string ConvertedFirstArr = string.Join(",", firstArr);
+        string ConvertedSecondArr = string.Join(",", secArr);
+        if (ConvertedSecondArr.Length > ConvertedFirstArr.Length)
+        {
+            Console.WriteLine("Size is not aviable");
+
+        }
+        else if (ConvertedSecondArr.Length <= ConvertedFirstArr.Length)
+        {
+            if(ConvertedFirstArr.Contains(ConvertedSecondArr))
+                return true;
+        }
+                return false;
+        }
+    
+
+  }
